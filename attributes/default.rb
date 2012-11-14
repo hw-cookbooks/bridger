b87b58a3b3fb243835ee4c0e52df0af0bbd90f76
Chris Roberts <chrisroberts.code@gmail.com>
2012-11-14 14:32:07 -0800

    Add on boot setup helpers. Restructure idempotency detection.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,7 @@ default[:bridger][:dhcp] = true
 default[:bridger][:address] = nil
 default[:bridger][:netmask] = '255.255.255.0'
 default[:bridger][:gateway] = nil
+default[:bridger][:enable_on_boot] = false
 
 # Additional bridges are hashes using the same
 # infomation structure as above
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,40 @@
 package 'bridge-utils'
 
+if(node[:bridger][:enable_on_boot])
+  # setup startup bits
+  directory '/etc/bridger'
+
+  file '/etc/bridger/bridge.cnf' do
+    action :create
+    content(
+      JSON.pretty_generate(
+        [{:interface => node[:bridger][:interface], :name => node[:bridger][:name], 
+        :dhcp => node[:bridger][:dhcp], :address => node[:bridger][:address],
+        :netmask => node[:bridger][:netmask], :gateway => node[:bridger][:gateway]
+        }] + node[:bridger][:additionals]
+      )
+    )
+    mode 0600
+  end
+
+  template '/etc/init.d/bridger-setup' do
+    mode 0755
+  end
+
+  template '/usr/local/bin/bridger-init' do
+    source 'bridger.rb.erb'
+    variables(
+      :config_file => '/etc/bridger/bridge.cnf',
+      :ruby_path => File.join(
+        RbConfig::CONFIG['bindir'],
+        RbConfig::CONFIG['ruby_install_name']
+      )
+    )
+    mode 0755
+  end
+end
+
+# now check current state
 ([node[:bridger]] + node[:bridger][:additionals]).each do |bridge|
   # sanity checks
   if(bridge[:address] && bridge[:dhcp])
@@ -23,23 +58,36 @@ package 'bridge-utils'
     subscribes :run, resources(:execute => "bridger[kill the interface (#{bridge[:interface]})]"), :immediately
   end
 
-  execute "bridger[bind the bridge (#{bridge[:name]} -> #{bridge[:interface]})]" do
-    command "brctl addif #{bridge[:name]} #{bridge[:interface]}"
-    action :nothing
-    subscribes :run, resources(:execute => "bridger[kill the interface (#{bridge[:interface]})]"), :immediately
+  ruby_block "bridger[interface bind notifier for #{bridge[:name]}]" do
+    block do
+      true
+    end
+  end
+
+  Array(bridge[:interface]).each do |interface|
+    execute "bridger[bind the bridge (#{bridge[:name]} -> #{interface})]" do
+      command "brctl addif #{bridge[:name]} #{interface}"
+      not_if do
+        %x{
+          brctl show #{bridge[:name]} | grep #{bridge[:name]}
+        }.strip.split("\n").detect do |line|
+          line.split(/\s/).last == interface
+        end
+      end
+    end
   end
 
   if(bridge[:dhcp])
     execute "bridger[configure the bridge (#{bridge[:name]} - dynamic)]" do
       command "dhclient #{bridge[:name]}"
       action :nothing
-      subscribes :run, resources(:execute => "bridger[kill the interface (#{bridge[:interface]})]"), :immediately
+      subscribes :run, resources(:execute => "bridger[create the bridge (#{bridge[:name]})]"), :immediately
     end
   else
     execute "bridger[configure the bridge (#{bridge[:name]} - static)]" do
       command "ifconfig #{bridge[:name]} #{bridge[:address]} netmask #{bridge[:netmask]}"
       action :nothing
-      subscribes :run, resources(:execute => "bridger[kill the interface (#{bridge[:interface]})]"), :immediately
+      subscribes :run, resources(:execute => "bridger[create the bridge (#{bridge[:name]})]"), :immediately
     end
   end
   # YAY we built a bridge!
